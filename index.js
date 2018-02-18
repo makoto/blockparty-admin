0c2feaf05135f730b3e0959f260796b7b6599f87
Makoto <makoto@Makotos-Air.lan>
2018-02-18 22:00:47 +0000

    Connect to infura for each request

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const Web3 = require('web3');
-const web3 = new Web3("wss://mainnet.infura.io/ws");
 const secret_file =  'tmp/test_private.key';
 const privateKey = process.env.SECRET || fs.readFileSync(secret_file, 'utf8');
 const moment = require('moment');
@@ -12,16 +11,18 @@ const address = data.networks['1'].address;
 
 require('http').createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const web3 = new Web3("wss://mainnet.infura.io/ws");
     var conference = new web3.eth.Contract(abi, address)
     conference.methods.name().call(function(e,name){
+        console.log(e, name);
         if(name){
             res.write(name.toString());
             res.write('\n');
         }
 
         conference.getPastEvents('RegisterEvent', {fromBlock:5080322}, function(error, events){
-            console.log(r)
-            console.log(events.length)
+            console.log(error);
+            console.log(events.length);
             events.forEach(function(event, index){
                 let decrypted = crypto.privateDecrypt(privateKey, new Buffer(event.returnValues._encryption, 'hex')).toString('utf8');
                 web3.eth.getBlock(event.blockNumber).then(function(r){
